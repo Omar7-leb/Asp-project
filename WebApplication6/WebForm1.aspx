342fd3a608aa43a1376ce988b0013cf59e043cb2
Omar7-leb <125736709+Omar7-leb@users.noreply.github.com>
2023-07-08 09:44:24 +0300

    project

---
+++ WebApplication6/WebForm1.aspx
@@ -0,0 +1,202 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication6.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<title></title>
+<style type="text/css">
+.style1
+{
+font-family: "Bahnschrift SemiBold";
+}
+.style2
+{
+width: 100%;
+height: 252px;
+}
+.style3
+{
+color: #FF0000;
+font-style: italic;
+font-weight: bold;
+}
+.style6
+{
+height: 145px;
+}
+.style8
+{
+height: 145px;
+text-align: center;
+}
+.style9
+{
+text-align: center;
+}
+.style10
+{
+font-weight: bold;
+}
+.style11
+{
+width: 100%;
+}
+.style12
+{
+width: 247px;
+}
+.style13
+{width: 247px;
+text-align: center;
+}
+</style>
+</head>
+<body>
+<form id="form1" runat="server">
+<div class="style1">
+<h1>
+<strong style="font-family: Bahnschrift; font-style:
+normal; color: #000000; font-size: large;">
+Inscription in 3rd year Computer Science:</strong></h1>
+</div>
+<table class="style2">
+<tr>
+<td>
+<strong>Course Code :</strong></td>
+<td>
+<strong>
+<asp:TextBox ID="txtcourse" runat="server"
+CssClass="style10" pattern="[A-Za-z]{1}[0-9]{4}"></asp:TextBox>
+</strong>
+</td>
+<td>
+<strong>
+<asp:RequiredFieldValidator ID="RequiredFieldValidator1"
+runat="server"
+ControlToValidate="txtcourse" CssClass="style3"
+Display="Dynamic"
+ErrorMessage="Course Code is obligatory
+"></asp:RequiredFieldValidator>
+</strong>
+</td>
+</tr>
+<tr>
+<td>
+<strong>Credit Number :</strong></td>
+<td>
+<strong>
+<asp:TextBox ID="txtcredit" runat="server"
+CssClass="style10"></asp:TextBox>
+</strong>
+</td>
+<td>
+<strong>
+<asp:RangeValidator ID="RangeValidator1" runat="server"
+ControlToValidate="txtcredit" CssClass="style3"
+Display="Dynamic"
+ErrorMessage="Credit nb between 3 and 6"
+MaximumValue="6" MinimumValue="3"></asp:RangeValidator>
+</strong>
+</td>
+</tr>
+<tr>
+<td class="style9">
+<strong><asp:Button ID="btnAdd" runat="server" CssClass="style10"
+Text="Add"
+onclick="btnAdd_Click" />
+</strong>
+</td>
+<td>
+&nbsp;</td>
+<td class="style9">
+<strong>
+<asp:Button ID="btnClear" runat="server"
+CssClass="style10" Text="Clear"
+onclick="btnClear_Click" />
+</strong>
+</td>
+</tr>
+<tr>
+<td class="style6">
+<strong>List of Models : </strong>
+</td>
+<td class="style6">
+<strong>
+<asp:ListBox ID="listofmodules" runat="server" style="textalign: center"
+Width="89px" SelectionMode="Multiple"
+CssClass="style10">
+<asp:ListItem Value="0">--Code--</asp:ListItem>
+</asp:ListBox>
+</strong>
+</td>
+<td class="style8">
+<strong>
+<asp:Button ID="btnRegister" runat="server"
+CssClass="style10"
+Text="Register" onclick="btnRegister_Click" />
+</strong>
+</td>
+</tr>
+</table>
+<h1>
+<strong style="font-family: Arial; font-style:
+normal; color: #000000; font-size: large;">
+Summary :</strong></h1>
+<table class="style11">
+<tr>
+<td class="style12">
+<strong>Registred Courses :</strong></td>
+<td>
+<strong>
+<asp:ListBox ID="listofcourses" runat="server"
+SelectionMode="Multiple"
+CssClass="style10" Width="87px">
+<asp:ListItem Value="0">--Code--</asp:ListItem>
+</asp:ListBox>
+</strong>
+</td>
+<td><strong>Total nb of Credits :</strong></td>
+<td>
+<strong>
+<asp:TextBox ID="labelcredit" runat="server" Enabled="False"
+style="color: #0000CC; text-align: center"
+Width="43px" CssClass="style10" BorderStyle="None"
+>0</asp:TextBox>
+</strong>
+</td>
+</tr>
+<tr>
+<td class="style13">
+<strong>
+<asp:Button ID="btnDelete" runat="server"
+CssClass="style10"
+onclick="btnDelete_Click" Text="Delete" />
+</strong>
+</td>
+<td class="style9">
+<strong>
+<asp:Button ID="btnDeleteAll" runat="server"
+CssClass="style10"
+onclick="btnDeleteAll_Click" Text="Delete All" />
+</strong>
+</td>
+<td class="style9">
+<strong>
+<asp:Button ID="btnUP" runat="server" CssClass="style10"
+Text="Up"
+onclick="btnUP_Click" />
+</strong>
+</td>
+<td class="style9">
+<strong>
+<asp:Button ID="btnDown" runat="server"
+CssClass="style10" Text="Down"
+onclick="btnDown_Click" />
+</strong>
+</td>
+</tr>
+</table>
+</form>
+</body>
+</html>
---
+++ WebApplication6/WebForm1.aspx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication6
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e) // clear button
+
+        {
+            txtcourse.Text = " "; // Setting course text box to empty
+            txtcredit.Text = "0"; // Setting credit text box to 0
+        }
+        protected void btnDelete_Click(object sender, EventArgs e) //Delete Selected registered Course
+
+        {
+            if (listofcourses.SelectedItem.Text != "--Code--") // check if its not the code
+
+            {
+                ListItem Delete = listofcourses.SelectedItem; // find the  selected one
+
+                listofcourses.Items.Remove(Delete);// remove it from the list
+
+                int totalCredit = Convert.ToInt32(this.labelcredit.Text);// to get credit nb
+
+                string credit1 = Delete.Value;
+                string[] credit = credit1.Split(new string[] { "_elie,"}, StringSplitOptions.None);
+                totalCredit = totalCredit - int.Parse(credit[1]);// to  reduce credit nb
+
+                this.labelcredit.Text = totalCredit.ToString();//to reset credit nb
+
+            }
+        }
+        protected void btnDeleteAll_Click(object sender, EventArgs e)
+        {
+            labelcredit.Text = "0";
+            listofcourses.Items.Clear();//to clear the list
+            ListItem code = new ListItem("--Code--", "0");// to add the  list item code(fixed one)
+
+            listofcourses.Items.Add(code);
+        }
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            string course = txtcourse.Text;
+            string credit = txtcredit.Text; if (course.Length == 5)// check if course digit are five
+            { // if true we want to add
+                ListItem item = new ListItem();
+                item.Text = course;
+                item.Value = course + "_elie," + credit;// to get a unique value for each listitem
+
+                if (!listofmodules.Items.Contains(item))
+                { //check if the course is already part of the list
+
+                    listofmodules.Items.Add(item);
+                }
+            }
+            else { txtcourse.Text = "course should be 5 digits"; }
+        }
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            int totalCredit = Convert.ToInt32(this.labelcredit.Text);// total number of credits
+
+            int pos = listofmodules.SelectedIndex; // getting selected item position
+
+            if (!listofcourses.Items.Contains(listofmodules.SelectedItem))// if item already in list of registred items
+            {
+
+                string credit1 = listofmodules.SelectedItem.Value; // getting the unique value before extracting credit nb
+
+
+                string[] credit = credit1.Split(new string[] { "_elie," }, StringSplitOptions.None);// getting the credit number from the  unique value
+
+                totalCredit = totalCredit + int.Parse(credit[1]); //  calculating the new credit number
+
+                if (totalCredit < 37) // if less than 36 do the work
+
+                { // if all coditons are true we add it
+                    listofcourses.Items.Add(listofmodules.SelectedItem);
+                     this.labelcredit.Text = totalCredit.ToString();
+                }
+            }
+        }
+        protected void btnUP_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listofcourses.SelectedIndex;
+            if (listofcourses.SelectedIndex == 1) // if the item is the item under the string “—code—"
+
+            {
+                return;
+            }
+            else
+            {
+                if (selectedIndex > 0) // if the selected item is in the listbox
+                {
+                    {
+                        listofcourses.Items.Insert
+                        (selectedIndex - 1, listofcourses.Items
+                        [selectedIndex]);
+                        listofcourses.Items.RemoveAt(selectedIndex
+                        + 1); listofcourses.SelectedIndex =
+                        selectedIndex - 1;
+                    }
+                }
+            }
+        }
+        protected void btnDown_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listofcourses.SelectedIndex;
+            if (selectedIndex < listofcourses.Items.Count - 1 && selectedIndex != 0 && selectedIndex != -1)
+            {
+                listofcourses.Items.Insert(selectedIndex + 2,
+                listofcourses.Items[selectedIndex]);
+                listofcourses.Items.RemoveAt(selectedIndex);
+                listofcourses.SelectedIndex = selectedIndex + 1;
+            }
+        }
+    }
+}
